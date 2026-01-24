2756aaaac521e930f40389a07ec3347e5e0162e8
Felipeu28 <143538765+Felipeu28@users.noreply.github.com>
2026-01-24 08:30:17 -0600

    Update index.ts

--- supabase/functions/create-checkout/index.ts
+++ supabase/functions/create-checkout/index.ts
@@ -1,7 +1,7 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import Stripe from 'https://esm.sh/stripe@14.0.0?target=deno';
-import { corsHeaders, handleCors } from '../_shared/cors.ts';
+import { corsHeaders, handleCors } from '../shared/cors.ts';
 
 console.log('Create Checkout Function Started');
 
